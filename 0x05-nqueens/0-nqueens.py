719e2802d96b8d17262f5bc68358304a89a6320d
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-05 21:50:10 +0200

    queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""N queens"""
+
+import sys
+from typing import List
+
+
+def nqueens(n: int) -> List[List[int]]:
+    """
+    nqueens
+    """
+    col = set()
+    posDiag = set()
+    negDiag = set()
+    board = []
+    res = []
+
+    def backtrack(r: int) -> List:
+        """
+        backtrack
+        """
+        if r == n:
+            copy = board.copy()
+            res.append(copy)
+            return
+        for c in range(n):
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                continue
+            col.add(c)
+            posDiag.add(r + c)
+            negDiag.add(r - c)
+            board.append([r, c])
+            backtrack(r + 1)
+            col.remove(c)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
+            board.remove([r, c])
+
+    backtrack(0)
+    return res
+
+
+if __name__ == "__main__":
+    if not sys.argv[1].isnumeric():
+        print("N must be a number")
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+    queens = nqueens(int(sys.argv[1]))
+    for i in queens:
+        print(i)
