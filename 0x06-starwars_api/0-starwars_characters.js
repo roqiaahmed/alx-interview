0671d5e15fe1fe3cf147de66890eceb9faa736fb
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-16 19:21:29 +0200

    done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(url, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    const result = await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        if (err) reject(err);
+        else resolve(JSON.parse(body).name);
+      });
+    });
+    console.log(result);
+  }
+});
