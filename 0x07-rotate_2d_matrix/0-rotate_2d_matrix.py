07b65007874c2b1ef15a754dc58aaa94f240f975
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-21 12:50:27 +0200

    rotate_2D

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    for r in range(n):
+        for c in range(r, n):
+            matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+    for r in range(n):
+        matrix[r].reverse()
