1be8605cb3117f0fb78c8e24d607a6277d8dd788
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-03-09 08:35:41 +0200

    f

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,7 +11,6 @@ def island_perimeter(grid):
     rows = len(grid)
     cols = len(grid[0])
     dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    # stack = []
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
@@ -22,24 +21,4 @@ def island_perimeter(grid):
                         or grid[new_row][new_col] == 0
                     ):
                         perimeter += 1
-    # for row in range(rows):
-    #     for col in range(cols):
-    #         if grid[row][col] == 1:
-    #             stack.append((row, col))
-    #             break
-    #     if len(stack) != 0:
-    #         break
-    # while stack:
-    #     row, col = stack.pop()
-    #     grid[row][col] = "A"
-    #     for n_row, n_col in dir:
-    #         new_row = row + n_row
-    #         new_col = col + n_col
-    #         if (0 <= new_row < rows) and (0 <= new_col < cols):
-    #             if grid[new_row][new_col] == 1:
-    #                 stack.append((new_row, new_col))
-    #             if grid[new_row][new_col] == 0:
-    #                 perimeter += 1
-    #         else:
-    #             perimeter += 1
     return perimeter
